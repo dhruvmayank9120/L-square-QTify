a1b43ceb84078a8c4006db147350aeb3219ce619
dhruvmayank <mayankdhruv9120@gmail.com>
2026-02-25 04:25:31 +0530

    qtify-2

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,20 +1,18 @@
 import styles from "./Hero.module.css";
-import headphones from "../../assets/hero_headphones.png";
+import heroImage from "../../assets/hero_headphones.png";
 
 function Hero() {
   return (
     <div className={styles.hero}>
-  <div className={styles.textContainer}>
-    <h1>100 Thousand Songs, ad-free</h1>
-    <h1>Over thousands podcast episodes</h1>
-  </div>
+      <div className={styles.heroContent}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <p>Over thousands podcast episodes</p>
+      </div>
 
-  <img
-    className={styles.image}
-    src={headphones}
-    alt="headphones"
-  />
-</div>
+      <div className={styles.heroImage}>
+        <img src={heroImage} alt="headphones" />
+      </div>
+    </div>
   );
 }
 
